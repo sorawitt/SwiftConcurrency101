d579ce79bea281119de080c9b7519e8bb1c27b54
Sorawit Trutsat (Kong) <sorawit.tr@gmail.com>
2022-07-27 20:02:58 +0700

    first commit

---
+++ SwiftConcurrency101/SwiftConcurrency101App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftConcurrency101App.swift
+//  SwiftConcurrency101
+//
+//  Created by Sorawit Trutsat on 27/7/2565 BE.
+//
+
+import SwiftUI
+
+@main
+struct SwiftConcurrency101App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
